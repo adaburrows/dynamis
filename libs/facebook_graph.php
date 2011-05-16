86b8cc08969152d2c076d60208d2220b96029168
Jillian Ada Burrows <Jillian@RepresentationDesign.net>
2011-05-15 20:42:27 -0700

    Fixed token -> access_token

--- libs/facebook_graph.php
+++ libs/facebook_graph.php
@@ -120,7 +120,7 @@ class facebook_graph extends OAuth2 {
         $this->request_params['path'] = $object;
         $this->request_params['query_params'] = array(
             'metadata' => 1,
-            'access_token' => $this->token
+            'access_token' => $this->access_token
         );
         $object = $this->do_request() ? $this->get_data() : null;
         $meta = $this->_has_meta($object);
