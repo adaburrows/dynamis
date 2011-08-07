cfdde8531d01abef4dec4cd2b3e6accf01ee3762
Jillian Ada Burrows <jill@adaburrows.com>
2011-08-07 16:14:34 -0700

    No longer using any global functions or variables; majorly incompatible with previous versions.

--- core/app.php
+++ core/app.php
@@ -72,6 +72,102 @@ class app {
     }
 
 /*
+ * Utility methods.
+ * =============================================================================
+ */
+
+   /*
+    * app::url();
+    * -----------
+    * Returns the proper site url relative to the base url for the specified resource.
+    * Use in all views to retain portability across domains.
+    */
+    public static function url($url) {
+        // Is this supposed to be a secure URL?
+        $secure_url = app::$is_secure_url;
+        if (is_array($url)) {
+            $secure_url = router::isSecureRoute($url);
+            $url = router::unmap($url);
+        }
+        $proto = $secure_url ? 'https://' : 'http://';
+        $base = self::$config['site_base'] != '/' ? self::$config['site_base'] : $_SERVER['SERVER_NAME'];
+        return("{$proto}{$base}/{$url}");
+    }
+
+   /*
+    * app::http_s();
+    * --------------
+    * Return the proper http:// or https:// protocol
+    */
+    function http_s($url) {
+        // Is this supposed to be a secure URL?
+        $secure_url = app::$is_secure_url;
+        $proto = $secure_url ? 'https://' : 'http://';
+        return("{$proto}{$url}");
+    }
+
+   /*
+    * app::normalize_url();
+    * ---------------------
+    * Takes a recognizable url and makes sure it has a http(s?):// prefix to ensure all urls are never relative
+    */
+    function normalize_url($url) {
+        $url_pattern = "@\b(https?://)?(([0-9a-zA-Z_!~*'().&=+$%-]+:)?[0-9a-zA-Z_!~*'().&=+$%-]+\@)?(([0-9]{1,3}\.){3}[0-9]{1,3}|([0-9a-zA-Z_!~*'()-]+\.)*([0-9a-zA-Z][0-9a-zA-Z-]{0,61})?[0-9a-zA-Z]\.[a-zA-Z]{2,6})(:[0-9]{1,4})?((/[0-9a-zA-Z_!~*'().;?:\@&=+$,%#-]+)*/?)@";
+        if (preg_match($url_pattern, $url) > 0) {
+            if (preg_match('@\b(https?://)@', $url) == 0) {
+                $url = "http://$url";
+            }
+        } else {
+            $url = false;
+        }
+        return $url;
+    }
+
+   /*
+    * app::isPhonenumber();
+    * ---------------------
+    * checks to see if a number is in the right form to be phone number
+    */
+    function isPhoneNumber($number) {
+        $number = trim($number);
+        return (preg_match('/^((\+)?[0-9]( |-)?)?(\(?[0-9]{3}\)?|[0-9]{3})( |-)?([0-9]{3}( |-)?[0-9]{4})$/', $number));
+    }
+
+   /*
+    * app::formatNumber();
+    * --------------------
+    * Formats a number for display
+    * TODO: check number of digits and format accordingly
+    */
+    function formatPhoneNumber($number) {
+        return ('('.substr($number,0,3).') '.substr($number,3,3)."-".substr($number,6,4));
+    }
+
+   /*
+    * app::normalizePhoneNumber();
+    * ----------------------------
+    * returns only digits of a phone number
+    */
+    function normalizePhoneNumber($number) {
+        $number = trim($number);
+        $number = preg_replace('/(\+| |-|\(|\))?/', '', $number);
+        if(strlen($number) == 11) {
+            $number = substr($number, 1);
+        }
+        return $number;
+    }
+
+   /*
+    * app::_e();
+    * ----------
+    * For outputting values that might not be defined.
+    */
+    function _e($string) {
+        echo isset($string) ? $string : "";
+    }
+
+
+/*
  * Methods for loading classes from libraries, controllers, and models.
  * =============================================================================
  */
--- core/config.php
+++ core/config.php
@@ -1,24 +1,25 @@
 <?php
+
 /* ============================================================================
- * Bootstrap file
- * --------------
- * Loads all the required files for the app to work and gives it a slight kick! 
+ * class config;
+ * ------------
+ * This class is in charge of the configuration information.
  * ============================================================================
  * -- Version alpha 0.1 --
  * This code is being released under an MIT style license:
  *
  * Copyright (c) 2010 Jillian Ada Burrows
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,61 +27,58 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
- *------------------------------------------------------------------------------
+ * ------------------------------------------------------------------------------
  * Original Author: Jillian Ada Burrows
  * Email:           jill@adaburrows.com
  * Website:         <http://www.adaburrows.com>
  * Github:          <http://github.com/jburrows>
  * Facebook:        <http://www.facebook.com/jillian.burrows>
  * Twitter:         @jburrows
- *------------------------------------------------------------------------------
+ * ------------------------------------------------------------------------------
  * Use at your own peril! J/K
- * 
+ *
  */
 
-// Include config class - dependancy of eveything
-require_once BASEPATH.'core/config'.EXT;
-$start_time = config::load();
+class config {
+    protected static $config = array();
 
-// Include router - dependancy of app.
-require_once BASEPATH.'core/router'.EXT;
-// Setup routes for application
-require_once APPPATH.'routes'.EXT;
-
-// Include layout - dependancy of app.
-require_once BASEPATH.'core/layout'.EXT;
-
-// Include app
-require_once BASEPATH.'core/app'.EXT;
-app::setConfig(config::getAll());
-app::setStartTime($start_time);
-
-// Set the global error handler
-set_error_handler( array('app', 'error_handler') );
-// Set the global exception handler
-set_exception_handler( array('app', 'exception_handler') );
-// Set the global shutdown handler
-register_shutdown_function( array('app', 'shutdown_handler') );
-
-// Load core classes that all classes extend
-$core = config::get('core');
-if($core){
-    foreach($core as $class) {
-        if (file_exists(APPPATH."core/$class".EXT)) {
-            require_once APPPATH."core/$class".EXT;
-        } else if (file_exists(BASEPATH."core/$class".EXT)) {
-            require_once BASEPATH."core/$class".EXT;
+    public static function load() {
+        // Require the application configuration file
+        require_once APPPATH.'config'.EXT;
+        if (empty($config['timezone'])) {
+            $config['timezone'] = 'America/Los_Angeles';
         }
+        // Set up time
+        date_default_timezone_set($config['timezone']);
+        $start_time = microtime(true);
+        // Set up defaults
+        if(empty($config['default_request_type'])){
+            $config['default_request_type'] = 'html';
+        }
+        if(empty($config['default_controller'])){
+            $config['default_controller'] = 'main';
+        }
+        if(empty($config['default_method'])){
+            $config['default_method'] = 'index';
+        }
+        // Core libraries to load
+        if(empty ($config['core'])) {
+            $config['core'] = array('db', 'model');
+        }
+        self::$config = $config;
+
+        return $start_time;
+    }
+
+    public static function set($config_var, $value) {
+        self::$config[$config_var] = value;
+    }
+
+    public static function get($config_var) {
+        $value = false;
+        if(isset(self::$config[$config_var])) {
+            $value = self::$config[$config_var];
+        }
+        return $value;
     }
 }
-
-// Connect to database
-if(config::get('use_database')) {
-    if(is_file(APPPATH.'models/schema/aspects'.EXT)) {
-        require_once APPPATH.'models/schema/aspects'.EXT;
-    }
-    db::connect();
-}
-
-app::getCore('controller');
-app::go();
