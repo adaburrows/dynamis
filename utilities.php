2188fea99a5ad8345647cd78a27d0f86afc94667
Jillian Ada Burrows <Jillian@RepresentationDesign.net>
2011-08-26 00:26:34 -0700

    Removing global functions: added public static declarations to util class functions

--- utilities.php
+++ utilities.php
@@ -3,7 +3,7 @@ class util {
   /**
    * Takes a recognizable url and makes sure it has a http(s?):// prefix to ensure all urls are never relative
    */
-  function normalize_url($url) {
+  public static function normalize_url($url) {
     $url_pattern = "@\b(https?://)?(([0-9a-zA-Z_!~*'().&=+$%-]+:)?[0-9a-zA-Z_!~*'().&=+$%-]+\@)?(([0-9]{1,3}\.){3}[0-9]{1,3}|([0-9a-zA-Z_!~*'()-]+\.)*([0-9a-zA-Z][0-9a-zA-Z-]{0,61})?[0-9a-zA-Z]\.[a-zA-Z]{2,6})(:[0-9]{1,4})?((/[0-9a-zA-Z_!~*'().;?:\@&=+$,%#-]+)*/?)@";
     if (preg_match($url_pattern, $url) > 0) {
       if (preg_match('@\b(https?://)@', $url) == 0) {
@@ -18,7 +18,7 @@ class util {
   /**
    * checks to see if a number is in the right form to be phone number
    */
-  function isPhoneNumber($number) {
+  public static function isPhoneNumber($number) {
     $number = trim($number);
     return (preg_match('/^((\+)?[0-9]( |-)?)?(\(?[0-9]{3}\)?|[0-9]{3})( |-)?([0-9]{3}( |-)?[0-9]{4})$/', $number));
   }
@@ -27,7 +27,7 @@ class util {
    * Formats a number for display
    * TODO: check number of digits and format accordingly
    */
-  function formatPhoneNumber($number) {
+  public static function formatPhoneNumber($number) {
     $number = self::normalizePhoneNumber($number);
     return ('('.substr($number,0,3).') '.substr($number,3,3)."-".substr($number,6,4));
   }
@@ -35,7 +35,7 @@ class util {
   /**
    * returns only digits of a phone number
    */
-  function normalizePhoneNumber($number) {
+  public static function normalizePhoneNumber($number) {
     $number = trim($number);
     $number = preg_replace('/(\+| |-|\(|\))?/', '', $number);
     if(strlen($number) == 11) {
@@ -44,6 +44,7 @@ class util {
     return $number;
   }
 }
+
 /**
  * For outputting values that might not be defined.
  */
